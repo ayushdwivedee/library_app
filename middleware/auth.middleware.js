f560030fd8a914187f1cb2ada2698ed359748b07
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2024-08-14 16:14:42 +0530

    Auth middleware done

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,4 +1,34 @@
-const auth=(req,res,next)=>{
-    const token=req.headers.authorization.split(" ")[1]
-    if(!token)
-}
+const jwt = require("jsonwebtoken");
+const userModel = require("../schema/user.schema");
+const auth = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      msg: ` Token not found  `,
+    });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
+        if (err) {
+          return res.status(401).json({
+            msg: ` Invalid token `,
+          });
+        }
+        if (decoded) {
+          const userEmail = decoded.email;
+          const user = await userModel.findOne({ userEmail });
+          if (!user) {
+            return res.status(401).json({
+              msg: ` User Not found `,
+            });
+          }
+          req.user = user;
+          next();
+        }
+      });
+    }
+  } catch (error) {
+    console.log(`Error occured in auth middleware ${error}`);
+  }
+};
