6be9725147a0657fc5aebdaa803134198dc28cf8
ayushdwivedee <ayushbhartiya1494927@gmail.com>
2024-08-22 16:21:38 +0530

    user schema updated

--- schema/user.schema.js
+++ schema/user.schema.js
@@ -4,7 +4,7 @@ const userSchema=mongoose.Schema({
     name:{type:String,required:true},
     email:{type:String,required:true},
     password:{type:String,required:true},
-    roles:{type:[String],enm:["CREATOR","VIEWER","VIEW_ALL"],required:true}
+    roles:{type:[String],enm:["CREATOR","VIEWER","VIEW_ALL"],default:["VIEW_ALL"],required:true}
 },{versionKey:false})
 
 const userModel=mongoose.model("User",userSchema)
